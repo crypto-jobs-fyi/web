ba8067c363ff0f68bd6e1e14d0456dc13d10d665
yury-dubinin <yury.dubinin@gmail.com>
2023-07-30 19:41:15 +0200

    + Keyrock

--- src/company_logo.py
+++ src/company_logo.py
@@ -29,7 +29,7 @@ def get_logo(company_name):
         'parity', 'kiln',
         'ankr', 'ultra',
         'solana', 'osmosisdex',
-        'ledger',
+        'ledger', 'Keyrock',
         'flashbots',
         'oplabs',
         'optimism',
